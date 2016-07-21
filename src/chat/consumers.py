75a4e5b19aa72b0ddb512849201a59661a61b2bd
Trezcool <kambembotresor@gmail.com>
2016-07-21 16:49:23 +0200

    Refactor consumer and client

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -10,75 +10,58 @@ log = logging.getLogger(__name__)
 
 class ChatConsumer(JsonWebsocketConsumer):
     http_user = True
+    room = None
+    receiver = None
 
     def __init__(self, message, **kwargs):
+        """
+        Performs some validations before initiating the connexion.
+        """
         try:
+            # Expected path formats: `/chat/p2p/1/` or `/chat/group/room-2/`
             prefix, chat_type, label = message['path'].strip('/').split('/')
             if prefix != 'chat' or chat_type not in ['p2p', 'group']:
                 log.debug('invalid ws path=%s', message['path'])
                 return
-            if chat_type == 'p2p' and not User.objects.filter(pk=label).exists():
-                log.debug('receiver does not exist id=%s', label)
-                return
-            if chat_type == 'group' and not Room.objects.filter(label=label).exists():
-                log.debug('ws room does not exist label=%s', label)
-                return
-            self.chat_type = chat_type
+            if chat_type == 'p2p':  # Person-to-person chat: check that a receiver with this ID exists.
+                try:
+                    self.receiver = User.objects.get(pk=label)
+                except User.DoesNotExist:
+                    log.debug('ws receiver does not exist id=%s', label)
+                    return
+            elif chat_type == 'group':  # Group chat: check that a room with this label exists.
+                try:
+                    self.room = Room.objects.get(label=label)
+                except Room.DoesNotExist:
+                    log.debug('ws room does not exist label=%s', label)
+                    return
             self.label = label
         except ValueError:
             log.debug('invalid ws path=%s', message['path'])
             return
         super().__init__(message, **kwargs)
 
-    @staticmethod
-    def get_p2p_label(pk1, pk2):
-        sorted_pk_list = sorted(map(str, [pk1, pk2]))
-        return '-'.join(sorted_pk_list)
-
     def connection_groups(self, **kwargs):
-        if self.chat_type == 'p2p':  # person-to-person chat
-            p2p_label = 'p2p-{}'.format(self.get_p2p_label(self.message.user.pk, self.label))
-            return [p2p_label]
+        """
+        Returns the group name based on the chat type.
+        """
+        if self.receiver:  # Person-to-person chat
+            p2p_label = '-'.join(sorted(map(str, [self.message.user.pk, self.label])))
+            return ['p2p-{}'.format(p2p_label)]
         return ['group-{}'.format(self.label)]
 
-    def connect(self, message, **kwargs):
-        self.message.channel_session['sender'] = message.user.pk
-        self.message.channel_session['group_name'] = self.connection_groups()[0]
-        if self.chat_type == 'p2p':  # person-to-person chat
-            self.message.channel_session['receiver'] = self.label
-            return
-        self.message.channel_session['room'] = self.label
-
     def receive(self, content, **kwargs):
-        room = label = receiver = None
-        session = self.message.channel_session
-        try:
-            label = session['room']
-            room = Room.objects.get(label=label)
-        except KeyError:  # Not a group chat
-            try:
-                label = session['receiver']
-                receiver = User.objects.get(pk=label)
-            except User.DoesNotExist:
-                log.debug('received message, user does not exist id=%s', label)
-                return
-        except Room.DoesNotExist:
-            log.debug('received message, room does not exist label=%s', label)
-            return
+        """
+        Saves the message in the DB and echo it back to the group channel.
+        """
         if set(content.keys()) != {'message'}:
             log.debug("ws message unexpected format data=%s", content)
             return
         if content:
-            try:
-                sender = User.objects.get(pk=session['sender'])
-            except User.DoesNotExist:
-                return
-            content.update({'sender': sender})
-            if room:  # Group chat
-                # log.debug('chat message room=%s handle=%s message=%s', room.label, data['handle'], data['message'])
-                m = room.messages.create(**content)
-            else:  # person-to-person chat
-                content.update({'receiver': receiver})
-                # log.debug('chat message room=%s handle=%s message=%s', room.label, data['handle'], data['message'])
+            content.update({'sender': self.message.user})
+            if self.room:  # Group chat
+                m = self.room.messages.create(**content)
+            else:  # Person-to-person chat
+                content.update({'receiver': self.receiver})
                 m = Message.objects.create(**content)
-            self.group_send(session['group_name'], m.as_dict())
+            self.group_send(self.connection_groups()[0], m.as_dict())
--- src/chat/views.py
+++ src/chat/views.py
@@ -7,7 +7,7 @@ from chat.models import Room, Message
 
 def chat_room(request, label=None, pk=None):
     """
-    Room view - show the room, with latest messages.
+    Show the chat page, with latest messages.
     The template for this view has the WebSocket business to send and stream
     messages, so see the template for where the magic happens.
     """
